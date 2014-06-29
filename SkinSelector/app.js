35ccfd29518f1e62c6b8176889d71fb6e1e3804b
IncoCode <akulich.dimon@gmail.com>
2014-06-29 14:22:32 +0300

    implemented replace in materials.cs

--- SkinSelector/app.js
+++ SkinSelector/app.js
@@ -1,7 +1,44 @@
 function SkinSelector(){}
 
+function changeMatFile(vehicleDir, skinName) {
+	//var code = 'function getSkinsFiles(%dir) {%i = 0; for( %file = findFirstFile( %dir @ "/*.dds" ); %file !$= ""; %file = findNextFile() ) { %files[%i] = %file; %i++; } return %files[0]; }';
+	//var code = 'function getSkinsFiles(%dir) { %file = findFirstFile( %dir @ "/*.dds" ); %files[0] = "ss"; return %files[0]; }';
+	var code = 'function getSkinsFiles(%vehicleDir, %skinDir) { %i = 0; for( %file = findFirstFile( %skinDir @ "/*.dds" ); %file !$= ""; %file = findNextFile() ) { %i++; %str = ""; %filename = strreplace( %file, %skinDir @ "/", "" ); %fileReader = new FileObject(); %fileReader.openForRead(%vehicleDir @ "materials.cs"); while ( !%fileReader.isEOF() ) { %tmp = %str; %line = %fileReader.readLine(); %str = %tmp NL %line; %text = strreplace( %str, %vehicleDir @ %filename, %file ); /*return %vehicleDir @ %filename @ " = " @ %file;*/ %str = %text; } %fileReader.close(); %fileReader = new FileObject(); %fileReader.openForWrite(%vehicleDir @ "materials.cs"); %fileReader.writeLine(%text); %fileReader.close(); return "yep"; } return "nope"; }';
+	executeGameEngineCode( code );
+	console.log(vehicleDir +"skins/" +skinName);
+	var skinDir = vehicleDir +"skins/" +skinName;
+	callGameEngineFuncCallback('getSkinsFiles("' +vehicleDir +'", "' +skinDir +'")', function(result) {
+		console.log('skins=' +result);
+	});			
+}
+
+function selectSkin(skinName) {
+	callLuaFuncCallback('v.vehicleDirectory', function(result) {		
+		var materialsFile = result +'materials.cs';
+		var defaultMaterialsFile = result +'skins/materials_def.cs';
+		console.log('matFile=' +materialsFile);
+		console.log('backMatFile=' +defaultMaterialsFile);
+		callGameEngineFuncCallback('isFile("' +defaultMaterialsFile +'")', function(res) {
+			console.log('res=' +res);
+			if (res == 0) {
+				callGameEngineFuncCallback('pathCopy("' +materialsFile +'", "' +defaultMaterialsFile +'", false)', function(res) {
+					console.log('backup done');
+				});
+			}
+			changeMatFile( result, skinName);
+		});
+	});
+}
+
 SkinSelector.prototype.initialize = function(){
 	$('<label id="lolz">lol</label>').appendTo(this.rootElement);
+	$('<button id="click_btn">3232</button>').appendTo(this.rootElement).click(function(){
+		console.log('click_btn click');
+		/*callGameEngineFuncCallback('pathCopy("vehicles/pigeon/pigeon_c.dds", "vehicles/pigeon/skins/pigeon_c.dds", false)', function(res) {
+			console.log('res=' +res);
+		});*/
+		selectSkin('Police');
+	});
 	$( document ).bind( "keydown", function(evt) {
 		if ( evt.ctrlKey && evt.keyCode == 66 )
 		{
