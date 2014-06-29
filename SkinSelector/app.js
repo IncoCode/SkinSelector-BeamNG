a63831d692342374fd34391669419cb960274855
IncoCode <akulich.dimon@gmail.com>
2014-06-29 15:21:17 +0300

    normal ui, fix size

--- SkinSelector/app.js
+++ SkinSelector/app.js
@@ -1,12 +1,14 @@
 function SkinSelector(){}
 
 function changeMatFile(vehicleDir, skinName) {	
-	var code = 'function getSkinsFiles(%vehicleDir, %skinDir) { %i = 0; if ( isFile( %vehicleDir @ "skins/materials_def.cs" ) ) { pathCopy( %vehicleDir @ "skins/materials_def.cs", %vehicleDir @ "materials.cs", false ); } else { return "nope"; } if ( endsWith( %skinDir, "/Default" ) ) { return "default"; } for( %file = findFirstFile( %skinDir @ "/*.dds" ); %file !$= ""; %file = findNextFile() ) { %i++; %str = ""; %filename = strreplace( %file, %skinDir @ "/", "" ); %fileReader = new FileObject(); %fileReader.openForRead( %vehicleDir @ "materials.cs" ); while ( !%fileReader.isEOF() ) { %tmp = %str; %line = %fileReader.readLine(); %str = %tmp NL %line; %text = strreplace( %str, %vehicleDir @ %filename, %file ); %str = %text; } %fileReader.close(); %fileReader = new FileObject(); %fileReader.openForWrite(%vehicleDir @ "materials.cs"); %fileReader.writeLine(%text); %fileReader.close(); } return "done"; }';
+	var code = 'function getSkinsFiles(%vehicleDir, %skinDir) { %i = 0; if ( isFile( %vehicleDir @ "skins/materials_def.cs" ) ) { pathCopy( %vehicleDir @ "skins/materials_def.cs", %vehicleDir @ "materials.cs", false ); } else { return "nope"; } if ( endsWith( %skinDir, "/Default" ) ) { return "default"; } for( %file = findFirstFile( %skinDir @ "/*.dds" ); %file !$= ""; %file = findNextFile() ) { %i++; %str = ""; %filename = strreplace( %file, %skinDir @ "/", "" ); %fileReader = new FileObject(); %fileReader.openForRead( %vehicleDir @ "materials.cs" ); while ( !%fileReader.isEOF() ) { %tmp = %str; %line = %fileReader.readLine(); %str = %tmp NL %line; %text = strreplace( %str, %vehicleDir @ %filename, %file ); %str = %text; } %fileReader.close(); %fileReader = new FileObject(); %fileReader.openForWrite(%vehicleDir @ "materials.cs"); %fileReader.writeLine(%text); %fileReader.close(); } return "done"; }'; // replacing in materials.cs
 	executeGameEngineCode( code );
-	console.log(vehicleDir +"skins/" +skinName);
+	//console.log(vehicleDir +"skins/" +skinName);
 	var skinDir = vehicleDir +"skins/" +skinName;
 	callGameEngineFuncCallback('getSkinsFiles("' +vehicleDir +'", "' +skinDir +'")', function(result) {
-		console.log('skins=' +result);
+		//console.log('skins=' +result);
+		callGameEngineFunc('beamNGReloadCurrentVehicle();');
+		$('.SkinSelector').parent().parent().hide( 'slow' );
 	});			
 }
 
@@ -14,10 +16,10 @@ function selectSkin(skinName) {
 	callLuaFuncCallback('v.vehicleDirectory', function(result) {		
 		var materialsFile = result +'materials.cs';
 		var defaultMaterialsFile = result +'skins/materials_def.cs';
-		console.log('matFile=' +materialsFile);
-		console.log('backMatFile=' +defaultMaterialsFile);
+		//console.log('matFile=' +materialsFile);
+		//console.log('backMatFile=' +defaultMaterialsFile);
 		callGameEngineFuncCallback('isFile("' +defaultMaterialsFile +'")', function(res) {
-			console.log('res=' +res);
+			//console.log('res=' +res);
 			if (res == 0) {
 				callGameEngineFuncCallback('pathCopy("' +materialsFile +'", "' +defaultMaterialsFile +'", false)', function(res) {
 					console.log('backup done');
@@ -28,22 +30,38 @@ function selectSkin(skinName) {
 	});
 }
 
+function updSkins() {
+	$('#skins')
+		.find('option')
+		.remove();
+	callLuaFuncCallback('v.vehicleDirectory', function(result) {		
+		callGameEngineFuncCallback( 'getDirectoryList("' +result +"skins/" +'")', function(arg) {
+			$('#skins').append('<option value="Default">Default</option>');
+			//console.log(arg);
+			var skinsArr = arg.split('	');
+			$('#skins').append(option);
+			for ( var i = 0; i < skinsArr.length; i++ ) {			
+				var option = $("<option></option>")
+					.attr("value", skinsArr[i])
+					.text(skinsArr[i]);
+				$('#skins').append(option);
+			}
+		});
+	});	
+}
+
 SkinSelector.prototype.initialize = function(){
-	$('<label id="lolz">lol</label>').appendTo(this.rootElement);
-	$('<button id="click_btn">3232</button>').appendTo(this.rootElement).click(function(){
-		console.log('click_btn click');
-		/*callGameEngineFuncCallback('pathCopy("vehicles/pigeon/pigeon_c.dds", "vehicles/pigeon/skins/pigeon_c.dds", false)', function(res) {
-			console.log('res=' +res);
-		});*/
-		selectSkin('Default');
+	$('<select id="skins"></select>').appendTo(this.rootElement);
+	$('<button id="selectSkin">Select skin</button>').appendTo(this.rootElement).click(function(){
+		console.log('selectSkin click');
+		var selectedSkin = $("#skins option:selected").val();
+		selectSkin( selectedSkin );
 	});
 	$( document ).bind( "keydown", function(evt) {
-		if ( evt.ctrlKey && evt.keyCode == 66 )
+		if ( evt.ctrlKey && evt.keyCode == 83 )
 		{
-			//$(this).hide();
-			//$("#lolz").hide();
-			console.log('1');
-			console.log($(this).width());
+			//console.log('1');
+			//console.log($(this).width());
 			if ($('.SkinSelector').is(':visible'))
 			{
 				$('.SkinSelector').parent().parent().hide( 'slow' );
@@ -51,13 +69,13 @@ SkinSelector.prototype.initialize = function(){
 			else
 			{
 				$('.SkinSelector').parent().parent().show( 'slow' );
+				updSkins();
 			}
 		}
-		//console.log('2');
-		//console.log(evt);
 	});
 		
 	console.log("SkinSelector inizialize");
+	updSkins();
 };
 
 SkinSelector.prototype.update = function(streams) {
