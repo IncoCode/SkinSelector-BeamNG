b1260392b9cd0f8938fb418428e213d2622f3748
IncoCode <akulich.dimon@gmail.com>
2014-06-30 01:32:33 +0300

    fixs

--- tc.cs
+++ tc.cs
@@ -1,5 +1,4 @@
-function getSkinsFiles(%vehicleDir, %skinDir) {
-	%i = 0;
+function getSkinsFiles(%vehicleDir, %skinDir) {	
 	if ( isFile( %vehicleDir @ "skins/materials_def.cs" ) ) {
 		pathCopy( %vehicleDir @ "skins/materials_def.cs", %vehicleDir @ "materials.cs", false );
 	}
@@ -9,25 +8,22 @@ function getSkinsFiles(%vehicleDir, %skinDir) {
 	if ( endsWith( %skinDir, "/Default" ) ) {
 		return "default";
 	}
-	for( %file = findFirstFile( %skinDir @ "/*.dds" ); %file !$= ""; %file = findNextFile() ) {
-		%i++;
-		%str = "";
-		%filename = strreplace( %file, %skinDir @ "/", "" );
-		
-		%fileReader = new FileObject();
-		%fileReader.openForRead( %vehicleDir @ "materials.cs" );
-		while ( !%fileReader.isEOF() ) {
-			%tmp = %str;
-			%line = %fileReader.readLine();
-			%str = %tmp NL %line;
-			%text = strreplace( %str, %vehicleDir @ %filename, %file );
-			%str = %text;
-		}
-		%fileReader.close();
-		%fileReader = new FileObject();
-		%fileReader.openForWrite(%vehicleDir @ "materials.cs");
-		%fileReader.writeLine(%text);
-		%fileReader.close();		
+	%str = "";
+	%fileReader = new FileObject();
+	%fileReader.openForRead( %vehicleDir @ "materials.cs" );
+	while ( !%fileReader.isEOF() ) {
+		%tmp = %str;
+		%line = %fileReader.readLine();
+		%str = %tmp NL %line;
 	}
+	%fileReader.close();
+	for( %file = findFirstFile( %skinDir @ "/*.dds" ); %file !$= ""; %file = findNextFile() ) {				
+		%filename = strreplace( %file, %skinDir @ "/", "" );
+		%str = strreplace( %str, %vehicleDir @ %filename, %file );
+	}
+	%fileReader = new FileObject();
+	%fileReader.openForWrite(%vehicleDir @ "materials.cs");
+	%fileReader.writeLine(%str);
+	%fileReader.close();	
 	return "done";
 }
