8f448fe5d187ea78c4945f7295cb031bc3e521dd
IncoCode <akulich.dimon@gmail.com>
2014-06-29 14:38:40 +0300

    added: restore default materials.cs

--- SkinSelector/app.js
+++ SkinSelector/app.js
@@ -1,9 +1,7 @@
 function SkinSelector(){}
 
-function changeMatFile(vehicleDir, skinName) {
-	//var code = 'function getSkinsFiles(%dir) {%i = 0; for( %file = findFirstFile( %dir @ "/*.dds" ); %file !$= ""; %file = findNextFile() ) { %files[%i] = %file; %i++; } return %files[0]; }';
-	//var code = 'function getSkinsFiles(%dir) { %file = findFirstFile( %dir @ "/*.dds" ); %files[0] = "ss"; return %files[0]; }';
-	var code = 'function getSkinsFiles(%vehicleDir, %skinDir) { %i = 0; for( %file = findFirstFile( %skinDir @ "/*.dds" ); %file !$= ""; %file = findNextFile() ) { %i++; %str = ""; %filename = strreplace( %file, %skinDir @ "/", "" ); %fileReader = new FileObject(); %fileReader.openForRead(%vehicleDir @ "materials.cs"); while ( !%fileReader.isEOF() ) { %tmp = %str; %line = %fileReader.readLine(); %str = %tmp NL %line; %text = strreplace( %str, %vehicleDir @ %filename, %file ); /*return %vehicleDir @ %filename @ " = " @ %file;*/ %str = %text; } %fileReader.close(); %fileReader = new FileObject(); %fileReader.openForWrite(%vehicleDir @ "materials.cs"); %fileReader.writeLine(%text); %fileReader.close(); return "yep"; } return "nope"; }';
+function changeMatFile(vehicleDir, skinName) {	
+	var code = 'function getSkinsFiles(%vehicleDir, %skinDir) { %i = 0; if ( isFile( %vehicleDir @ "skins/materials_def.cs" ) ) { pathCopy( %vehicleDir @ "skins/materials_def.cs", %vehicleDir @ "materials.cs", false ); } else { return "nope"; } if ( endsWith( %skinDir, "/Default" ) ) { return "default"; } for( %file = findFirstFile( %skinDir @ "/*.dds" ); %file !$= ""; %file = findNextFile() ) { %i++; %str = ""; %filename = strreplace( %file, %skinDir @ "/", "" ); %fileReader = new FileObject(); %fileReader.openForRead( %vehicleDir @ "materials.cs" ); while ( !%fileReader.isEOF() ) { %tmp = %str; %line = %fileReader.readLine(); %str = %tmp NL %line; %text = strreplace( %str, %vehicleDir @ %filename, %file ); %str = %text; } %fileReader.close(); %fileReader = new FileObject(); %fileReader.openForWrite(%vehicleDir @ "materials.cs"); %fileReader.writeLine(%text); %fileReader.close(); } return "done"; }';
 	executeGameEngineCode( code );
 	console.log(vehicleDir +"skins/" +skinName);
 	var skinDir = vehicleDir +"skins/" +skinName;
@@ -37,7 +35,7 @@ SkinSelector.prototype.initialize = function(){
 		/*callGameEngineFuncCallback('pathCopy("vehicles/pigeon/pigeon_c.dds", "vehicles/pigeon/skins/pigeon_c.dds", false)', function(res) {
 			console.log('res=' +res);
 		});*/
-		selectSkin('Police');
+		selectSkin('Default');
 	});
 	$( document ).bind( "keydown", function(evt) {
 		if ( evt.ctrlKey && evt.keyCode == 66 )
